bb64cb44e4e840cd4422de0e643aa9b0bf0c409b
andrey545454 <superkurlin2013@yandex.ru>
2018-07-15 07:38:02 +0500

    Kappa

--- messageHandler.py
+++ messageHandler.py
@@ -29,7 +29,7 @@ def damerau_levenshtein_distance(s1, s2):
 
 
 def load_modules():
-   files = os.listdir("/home/andrey545454/mysite/commands")
+   files = os.listdir(r"C:\Users\super\Desktop\Git-bot\os-sample-python\commands")
    modules = filter(lambda x: x.endswith('.py'), files)
    for m in modules:
        importlib.import_module("commands." + m[0:-3])
