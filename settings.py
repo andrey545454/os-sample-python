46f74fd99984228a3cd3242d27dfdb8dea2e4375
andrey545454 <superkurlin2013@yandex.ru>
2018-07-15 07:15:56 +0500

    Kappa

---
+++ settings.py
@@ -0,0 +1,2 @@
+token = '1316a6dbce30cd4f9dc64bc1c8e0a0d3248b40ca08b7d7758914c5ba5b87decaf96b97c0f8d09c876c5b3'
+confirmation_token = '00695f73'
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask,request,json
+from settings import *
+
 application = Flask(__name__)
 
-@application.route("/")
+@application.route("/",methods=['POST'])
+def proccessing():
+    data=json.loads(request.data)
+    if 'type' not in data.keys():
+        return 'not vk'
+    if data['type'] == 'confirmation':
+        return confirmation_token
+    elif data['type'] == 'message_new':
+        return 'ok'
+
+@application.route("/hello")
 def hello():
     return "Hello Flask bot!"
 
