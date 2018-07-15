24073141754d29f53a38e97390f1c5c452554d7c
andrey545454 <superkurlin2013@yandex.ru>
2018-07-15 23:52:40 +0500

    Kappa

--- stat_finder/rocketstats.py
+++ stat_finder/rocketstats.py
@@ -2,15 +2,36 @@ from rls.rocket import RocketLeague
 
 #работа с api от rocket league stats
 def checker_stats(text):
+    Ranks=['Unranked',
+           'Bronze I',
+           'Bronze II',
+           'Bronze III',
+           'Silver I',
+           'Silver II',
+           'Silver III',
+           'Gold I',
+           'Gold II',
+           'Gold III',
+           'Platinum I',
+           'Platinum II',
+           'Platinum III',
+           'Diamond I',
+           'Diamond II',
+           'Diamond III',
+           'Champion I',
+           'Champion II',
+           'Champion III',
+           'Grand Champion'
+           ]
     url=steam_url(text)
     rocket = RocketLeague(api_key='5B5H59SRROQSENSZAHNHJM2XQ1VFKK1O')
     info = rocket.players.player(id=url, platform=1).json()
     rankedSeasons = info['rankedSeasons']
     currentSeason = rankedSeasons[max(rankedSeasons)]
-    duelRank = currentSeason['10']['rankPoints']
-    doubleRank = currentSeason['11']['rankPoints']
-    soloStandartRank = currentSeason['12']['rankPoints']
-    StandartRank = currentSeason['13']['rankPoints']
+    duelRank = Ranks[currentSeason['10']['tier']]+'({})'.format(currentSeason['10']['rankPoints'])
+    doubleRank = Ranks[currentSeason['11']['tier']]+'({})'.format(currentSeason['11']['rankPoints'])
+    soloStandartRank = Ranks[currentSeason['12']['tier']]+'({})'.format(currentSeason['12']['rankPoints'])
+    StandartRank = Ranks[currentSeason['13']['tier']]+'({})'.format(currentSeason['13']['rankPoints'])
     return duelRank,doubleRank,soloStandartRank,StandartRank
 
 #парсер по ссылке
