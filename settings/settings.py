436e3b52e7f860fa95db3e4f2e44d311cfc8fd46
andrey545454 <superkurlin2013@yandex.ru>
2018-07-19 22:59:37 +0500

    Kappa

--- settings/settings.py
+++ settings/settings.py
@@ -1,3 +1,4 @@
 #файл с данными для работы бота с группой
-token = '1316a6dbce30cd4f9dc64bc1c8e0a0d3248b40ca08b7d7758914c5ba5b87decaf96b97c0f8d09c876c5b3'#токен группы
-confirmation_token = '00695f73'#токен для подтверждения
+import os
+token = str(os.environ.get('token'))#токен группы
+confirmation_token = str(os.environ.get('confirmation_token'))#токен для подтверждения
