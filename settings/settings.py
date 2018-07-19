76e5f2f0c0c0d2bd4cec44e32dad045f8ff7e807
andrey545454 <superkurlin2013@yandex.ru>
2018-07-20 00:21:11 +0500

    Kappa

--- settings/settings.py
+++ settings/settings.py
@@ -1,5 +1,5 @@
 # файл с данными для работы бота с группой
 import os
-token = str(os.environ.get('token'))  # ключ доступа к сообществу
-confirmation_token = str(os.environ.get('confirmation_token'))  # ключ для настройки сервера с callback api
-RLApi_token= str(os.environ.get('RLApi_token'))  # ключ для доступа к rocket league stats api
+token = os.environ.get('token')  # ключ доступа к сообществу
+confirmation_token = os.environ.get('confirmation_token')  # ключ для настройки сервера с callback api
+RLApi_token= os.environ.get('RLApi_token')  # ключ для доступа к rocket league stats api
