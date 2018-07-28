fbcdc5992389dcfec60ffe8dc7f15c3ac89155d3
andrey545454 <superkurlin2013@yandex.ru>
2018-07-28 18:11:36 +0500

    Kappa

--- stat_finder/datachecker.py
+++ stat_finder/datachecker.py
@@ -19,6 +19,7 @@ def admins_list_check(token, peer_id):  # Список админов бесед
                 if user.get('is_admin') is not None:
                     admins_list.append(user['member_id'])
         except:
-            print(user)  #check
+            print(user_list)
+            break
     del user_list
     return admins_list
