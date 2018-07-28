e5be9254c1b9a1412e1743abbae997469643cbb7
andrey545454 <superkurlin2013@yandex.ru>
2018-07-28 17:45:36 +0500

    Kappa

--- commands/link_to_stream.py
+++ commands/link_to_stream.py
@@ -1,9 +1,13 @@
 # команда для ссылки на стрим покса
 import command_system
+from stat_finder.datachecker import admins_list_check
 
 
 def link(token, user_id, stroka='', peer_id=''):
-    message = 'Ссылочка на стрим: '+r'https://www.twitch.tv/poqx'
+    if user_id in admins_list_check(token, peer_id):
+        message = 'Ссылка на твой стрим: https://www.twitch.tv/poqx'
+    else:
+        message = 'Ссылочка на стрим: https://www.twitch.tv/poqx'
     return message, ''
 
 
