9b7e45bd1abc43d6a865ecdabeab4b2237026b14
andrey545454 <superkurlin2013@yandex.ru>
2018-08-10 03:43:07 +0500

    Kappa

--- settings/BD.py
+++ settings/BD.py
@@ -1,13 +1,21 @@
+# Работа с бд (конкретнее с PostgreSQL)
 import psycopg2
 from os import environ
 
+# подключаемся к бд
 conn = psycopg2.connect(dbname=environ.get('POSTGRESQL_DATABASE'),
                         user=environ.get('POSTGRESQL_USER'),
                         password=environ.get('POSTGRESQL_PASSWORD'),
                         host=environ.get('POSTGRESQL_SERVICE_HOST'))
 cur = conn.cursor()
-cur.execute('Select * from bd;')
-for row in cur:
-    print(row)
+
+
+def get_info():
+    """достаём информацию из бд"""
+    cur.execute('Select * from bd;')
+    for row in cur:
+        print(row)
+
+
 cur.close()
 conn.close()
