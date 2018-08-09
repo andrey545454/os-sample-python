24ddaf73111d8c63a739a76cab34814dc03f103f
andrey545454 <superkurlin2013@yandex.ru>
2018-08-09 11:30:58 +0500

    Kappa

--- settings/BD.py
+++ settings/BD.py
@@ -1,9 +1,8 @@
 import psycopg2
 
-conn = psycopg2.connect(dbname='BD', user='andrey545454', password='andrey545454', host='10.129.104.250', port='5432')
+conn = psycopg2.connect(dbname='bd', user='andrey545454', password='andrey545454', host='10.129.104.250', port='5432')
 cur = conn.cursor()
-cur.execute('SELECT * FROM "BD";')
-
+cur.execute('SELECT * FROM bd;')
 print(cur.fetchall())
 cur.close()
 conn.close()
