20e0b460c0a4c19a8cf9271614be746c88cb3783
andrey545454 <superkurlin2013@yandex.ru>
2018-08-09 22:53:25 +0500

    Kappa

--- settings/BD.py
+++ settings/BD.py
@@ -1,9 +1,9 @@
 import psycopg2
 from os import environ
 
-conn = psycopg2.connect(user=environ.get('POSTGRESQL_USER'), password=environ.get('POSTGRESQL_PASSWORD'), host=environ.get('POSTGRESQL_SERVICE_HOST'))
+conn = psycopg2.connect(dbname=environ.get('POSTGRESQL_DATABASE'),user=environ.get('POSTGRESQL_USER'), password=environ.get('POSTGRESQL_PASSWORD'), host=environ.get('POSTGRESQL_SERVICE_HOST'))
 cur = conn.cursor()
-cur.execute("SELECT 1 FROM pg_database WHERE datname='{bd}'")
+cur.execute("SELECT 1 FROM pg_database WHERE datname='{}'".format(environ.get('POSTGRESQL_DATABASE')))
 print(cur.fetchone())
 cur.close()
 conn.close()
