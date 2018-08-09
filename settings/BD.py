bd6d9287fa9edbdccab4775f7eb488fd49a24f82
andrey545454 <superkurlin2013@yandex.ru>
2018-08-09 23:15:34 +0500

    Kappa

--- settings/BD.py
+++ settings/BD.py
@@ -1,9 +1,9 @@
 import psycopg2
 from os import environ
 
-conn = psycopg2.connect(dbname=environ.get('POSTGRESQL_DATABASE'),user=environ.get('POSTGRESQL_USER'), password=environ.get('POSTGRESQL_PASSWORD'), host=environ.get('POSTGRESQL_SERVICE_HOST'))
+conn = psycopg2.connect(dbname=environ.get('POSTGRESQL_DATABASE'), user=environ.get('POSTGRESQL_USER'), password=environ.get('POSTGRESQL_PASSWORD'), host=environ.get('POSTGRESQL_SERVICE_HOST'))
 cur = conn.cursor()
-cur.execute("SELECT * FROM {}".format(environ('POSTGRESQL_DATABASE')))
+cur.execute("SELECT * FROM {}".format(environ.get('POSTGRESQL_DATABASE')))
 print(cur.fetchone())
 cur.close()
 conn.close()
