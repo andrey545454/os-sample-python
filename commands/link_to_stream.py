21df801544217f3748c8088986494d7a109027ba
andrey545454 <superkurlin2013@yandex.ru>
2018-08-10 07:04:35 +0500

    Kappa

--- commands/link_to_stream.py
+++ commands/link_to_stream.py
@@ -5,20 +5,7 @@ from stat_finder.datachecker import admins_list_check,list_of_subs
 
 
 def link(token, user_id, stroka='', peer_id=''):
-    # проверка на покса чтобы запустить расссылку
-    if str(user_id) == '151635695':
-        # получаем список пользователей
-        list_of_users = list_of_subs(token, peer_id)['users']
-        message = 'Стример на Поксе подрубил стрим: https://www.twitch.tv/poqx'
-        # отправление сообщения каждому пользователю
-        for user in list_of_users:
-            # ловлю челов которые не писали боту?)
-            try:
-                send_message(user, user, token, message)
-            except:
-                pass
-    else:
-        message = 'Ссылочка на стрим: https://www.twitch.tv/poqx'
+    message = 'Ссылочка на стрим: https://www.twitch.tv/poqx'
     return message, ''
 
 
