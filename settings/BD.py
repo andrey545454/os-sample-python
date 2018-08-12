8d6e8bec206deda5237173429589bda1b3233117
andrey545454 <superkurlin2013@yandex.ru>
2018-08-12 20:34:49 +0500

    Kappa

--- settings/BD.py
+++ settings/BD.py
@@ -10,28 +10,28 @@ def connect():
                             password=environ.get('POSTGRESQL_PASSWORD'),
                             host=environ.get('POSTGRESQL_SERVICE_HOST'))
     cur = conn.cursor()
-    return cur
+    return cur, conn
 
 
 def get_info():
     """достаём информацию из бд"""
-    cur = connect()
+    cur, conn = connect()
     cur.execute('Select * from bd;')
     return cur.fetchall()
 
 
 def set_info(name, link):
     """добавляем информацию в бд"""
-    cur = connect()
+    cur, conn = connect()
     cur.execute('INSERT INTO bd (name, link, count) Values (%(name)s, %(link)s, %(count)s)', {'name': name,
                                                                                               'link': link,
                                                                                               'count': 1})
-    cur.commit()
+    conn.commit()
 
 
 def update_info(count, link):
     """обновляем информацию в бд"""
-    cur = connect()
+    cur, conn = connect()
     cur.execute('UPDATE bd SET count=(%(count)s) WHERE link=(%(link)s)', {'count': count+1,
                                                                      'link': link})
-    cur.commit()
+    conn.commit()
