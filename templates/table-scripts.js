c21e53d22960be40e51a7ee17bc91d25e4dda9f6
andrey545454 <superkurlin2013@yandex.ru>
2018-08-12 18:10:44 +0500

    Kappa

---
+++ templates/table-scripts.js
@@ -0,0 +1,5 @@
+$(function (){
+$('table td:first-child').each(function (i) {
+$(this).html(i+1);
+});
+});
--- templates/table.js
+++
@@ -1,4 +0,0 @@
-$('.table tbody tr').each(function(i) {
-var number = i + 1;
-$(this).find('td:first-child').text(number);
-});
