5fb25c63ff655bc1e91c11a2813cbae0e9dcd855
andrey545454 <superkurlin2013@yandex.ru>
2018-08-14 19:44:09 +0500

    Kappa

--- settings/BD.py
+++ settings/BD.py
@@ -35,4 +35,3 @@ def update_info(count, link):
     cur.execute('UPDATE bd SET count=(%(count)s) WHERE link=(%(link)s)', {'count': count+1,
                                                                           'link': link})
     conn.commit()
-
