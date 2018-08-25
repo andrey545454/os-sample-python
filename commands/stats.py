ba554487ceddef9b77c4fc09b6d4ec1adcb67d40
andrey545454 <superkurlin2013@yandex.ru>
2018-08-25 09:09:18 +0500

    Kappa

--- commands/stats.py
+++ commands/stats.py
@@ -6,7 +6,7 @@ from stat_finder.rocketstats import checker_stats
 def stats(token, user_id, stroka='', peer_id=''):
     try:
         mas = checker_stats(stroka)
-        message = '1c- '+str(mas[0])+'  ,2c- '+str(mas[1])+'  ,3cc- '+str(mas[2])+'  ,3c- '+str(mas[3])
+        message = '1c- '+str(mas[0])+'\n,2c- '+str(mas[1])+'\n,3cc- '+str(mas[2])+'\n,3c- '+str(mas[3])
     except:
         message = 'Что-то пошло не так!'
     return message, ''
