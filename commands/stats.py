14cebb81886b8f9ca1c0b24e908d9c17b9a1fd73
andrey545454 <superkurlin2013@yandex.ru>
2018-08-25 23:35:51 +0500

    Kappa

--- commands/stats.py
+++ commands/stats.py
@@ -8,11 +8,11 @@ def stats(token, user_id, stroka='', peer_id=''):
         mas = checker_stats(stroka)
         message = '1c- '+str(mas[0])+'\n,2c- '+str(mas[1])+'\n,3cc- '+str(mas[2])+'\n,3c- '+str(mas[3])
     except:
-        message = 'Что-то пошло не так!'
+        message = 'Не работает!'
     return message, ''
 
 
 stats_command = command_system.Command()
 stats_command.keys = ['!стата']
-stats_command.description = 'Узнать количество поинтов 1с, 2c, 3c, 3cc [нужно вставить ссылку на стим-аккаунт]'
+stats_command.description = 'Узнать количество поинтов 1с, 2c, 3c, 3cc [ Не работает :( ]'
 stats_command.process = stats
